139b0a0f6f708c242413f77a3b33a10c1137cef7
geonhee.sung <jvckigh@gmail.com>
2021-01-19 22:27:10 +0900

    AppConfig 리팩터링

--- src/main/java/com/slipper/springcorestudy/AppConfig.java
+++ src/main/java/com/slipper/springcorestudy/AppConfig.java
@@ -1,6 +1,8 @@
 package com.slipper.springcorestudy;
 
+import com.slipper.springcorestudy.discount.DiscountPolicy;
 import com.slipper.springcorestudy.discount.FixDiscountPolicy;
+import com.slipper.springcorestudy.member.MemberRepository;
 import com.slipper.springcorestudy.member.MemberService;
 import com.slipper.springcorestudy.member.MemberServiceImpl;
 import com.slipper.springcorestudy.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.slipper.springcorestudy.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
