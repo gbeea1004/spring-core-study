2d1ad70bab21dc8d767551cd4ba5208c27b138e2
geonhee.sung <jvckigh@gmail.com>
2021-01-24 23:36:45 +0900

    싱글톤 방식 주의점
    스프링 빈은 항상 무상태로 설계해야한다.

---
+++ src/test/java/com/slipper/springcorestudy/singleton/StatefulServiceTest.java
@@ -0,0 +1,31 @@
+package com.slipper.springcorestudy.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        int userAPrice = statefulService1.order("userA", 10000); // 스프링 빈은 항상 무상태로 설계하자!
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        assertThat(userAPrice).isEqualTo(10000);
+        assertThat(userBPrice).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
