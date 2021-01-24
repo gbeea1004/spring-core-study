f0687ca654e98c82b116669bed1aef379c2fad1b
geonhee.sung <jvckigh@gmail.com>
2021-01-25 00:11:22 +0900

    @Configuration을 지정하면 싱글톤
    스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용(임의의 다른클래스 생성)

---
+++ src/test/java/com/slipper/springcorestudy/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,31 @@
+package com.slipper.springcorestudy.singleton;
+
+import com.slipper.springcorestudy.AppConfig;
+import com.slipper.springcorestudy.member.MemberRepository;
+import com.slipper.springcorestudy.member.MemberServiceImpl;
+import com.slipper.springcorestudy.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
